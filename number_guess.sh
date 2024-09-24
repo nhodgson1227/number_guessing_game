f965209f4349a5cf17e04ecad962de9627b0d7a2
Nate Hodgson <nhodgson1227@gmail.com>
2024-09-24 21:14:22 +0000

    fix:sql dump to continue working on project

--- number_guess.sh
+++ number_guess.sh
@@ -9,7 +9,8 @@ SECRET_NUM=$RANDOM
 let "SECRET_NUM %= 1000"
 
 # Get User's name
-read -p "Enter your username: " MYNAME
+echo -e "Enter your username:"
+read MYNAME
 # Check for user in database
 USER_NAME=$($PSQL "SELECT user_name FROM players WHERE user_name = '$MYNAME'")
 # If user does not exist
@@ -17,33 +18,34 @@ if [[ -z $USER_NAME ]]
 then
   # create a new user
   INSERT_USER_RESULT=$($PSQL "INSERT INTO players(user_name) VALUES('$MYNAME')")
-  echo -e "\nWelcome, $MYNAME! It looks like this is your first time here."
+  echo -e "Welcome, $MYNAME! It looks like this is your first time here."
 else
   # get games played
-  # CODE
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE user_name = '$USER_NAME'")
   # get best game
-  # CODE
-  echo -e "\nWelcome back, $USER_NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+  BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE user_name = '$USER_NAME'")
+  echo -e "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
   #### ------------------ Guess the Number ------------------ ####
 
-while [ $GUESS -ne $SECRET_NUM ]; do
+while [[ $GUESS -ne $SECRET_NUM ]]; do
   # Read a new guess
-  read -p "Guess the secret number between 1 and 1000:" GUESS
+  echo "Guess the secret number between 1 and 1000:"
+  read GUESS
   # Check if input was an integer
   echo $GUESS
   # Increment Guesses
   NUMGUESS=$((NUMGUESS+1))
 
     # If higher
-  if [ $GUESS -gt $SECRET_NUM ]
+  if [[ $GUESS -gt $SECRET_NUM ]]
   then
     echo -e "\nIt's lower than that, guess again:"
   fi
 
   # If lower
-  if [ $GUESS -lt $SECRET_NUM ]
+  if [[ $GUESS -lt $SECRET_NUM ]]
   then
     echo -e "\nIt's higher than that, guess again:"
   fi
@@ -54,4 +56,9 @@ done
 echo -e "\nYou guessed it in $NUMGUESS tries. The secret number was $SECRET_NUM. Nice job!"
 
 # Increment Games Played on DB
+GAME_INC=$($PSQL "UPDATE players SET games_played=games_played + 1 WHERE user_name = '$USER_NAME'")
 # Check Best Game and Update if necessary
+if [[ $BEST_GAME -gt $NUMGUESS ]]
+then
+  BEST_INC=$($PSQL "UPDATE players SET best_game = $NUMGUESS WHERE user_name = '$USER_NAME'")
+fi
