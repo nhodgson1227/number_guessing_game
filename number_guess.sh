fc285845d6150a52a7a617b80daf97043502fe92
Nate Hodgson <nhodgson1227@gmail.com>
2024-09-25 16:15:50 +0000

    fix: Fixed logic for new users, feat:added integer verification

--- number_guess.sh
+++ number_guess.sh
@@ -8,33 +8,47 @@ GUESS=0
 SECRET_NUM=$RANDOM
 let "SECRET_NUM %= 1000"
 
+echo "$SECRET_NUM" #DEBUG
+
 # Get User's name
 echo -e "Enter your username:"
 read MYNAME
+
 # Check for user in database
 USER_NAME=$($PSQL "SELECT user_name FROM players WHERE user_name = '$MYNAME'")
 # If user does not exist
 if [[ -z $USER_NAME ]]
 then
   # create a new user
-  INSERT_USER_RESULT=$($PSQL "INSERT INTO players(user_name) VALUES('$MYNAME')")
-  echo -e "Welcome, $MYNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO players(user_name, games_played, best_game) VALUES('$MYNAME', 0, 0)")
+  echo -e "InsertUser: $INSERT_USER_RESULT" >> number_guess.log
+  USER_NAME=$MYNAME
+  echo -e "Welcome, $USER_NAME! It looks like this is your first time here."
+  BEST_GAME=1000
+  GAMES_PLAYED=0
 else
   # get games played
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE user_name = '$USER_NAME'")
   # get best game
   BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE user_name = '$USER_NAME'")
+  echo -e "Games_Played: $GAMES_PLAYED , Best_Game: $BEST_GAME" >> number_guess.log
   echo -e "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
   #### ------------------ Guess the Number ------------------ ####
 
-while [[ $GUESS -ne $SECRET_NUM ]]; do
+while [[ $GUESS -ne $SECRET_NUM ]]; 
+do
   # Read a new guess
   echo "Guess the secret number between 1 and 1000:"
   read GUESS
   # Check if input was an integer
-  echo $GUESS
+  while [[ $((GUESS)) != $GUESS ]]; 
+  do
+    echo "That is not an integer, guess again:"
+    read GUESS
+  done 
+
   # Increment Guesses
   NUMGUESS=$((NUMGUESS+1))
 
@@ -52,13 +66,17 @@ while [[ $GUESS -ne $SECRET_NUM ]]; do
 
 done
 
- # When correct, loop will exit.
-echo -e "\nYou guessed it in $NUMGUESS tries. The secret number was $SECRET_NUM. Nice job!"
-
 # Increment Games Played on DB
 GAME_INC=$($PSQL "UPDATE players SET games_played=games_played + 1 WHERE user_name = '$USER_NAME'")
+
 # Check Best Game and Update if necessary
 if [[ $BEST_GAME -gt $NUMGUESS ]]
 then
   BEST_INC=$($PSQL "UPDATE players SET best_game = $NUMGUESS WHERE user_name = '$USER_NAME'")
+  BEST_GAME=$NUMGUESS
 fi
+
+#echo -e "MYNAME: $MYNAME USERNAME: $USER_NAME NumGuess: $NUMGUESS BestGame: $BEST_GAME \n" >> number_guess.log
+
+echo "You guessed it in $NUMGUESS tries. The secret number was $SECRET_NUM. Nice job!"
+exit
